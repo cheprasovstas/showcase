a27336a95b83d1569d700c51608fea6bcef05dd3
u16783560 <Cheprasov.S.Al@sberbank.ru>
2020-10-09 17:03:34 +0300

    save

--- showcase/products/models.py
+++ showcase/products/models.py
@@ -5,16 +5,31 @@ from djmoney.models.fields import MoneyField
 from PIL import Image
 
 
-def upload_to(instance, filename):
+def upload_product_image_to(instance, filename):
     return 'images/products/%s/%s' % (instance.owner.id, filename)
+
+
+def upload_showcase_image_to(instance, filename):
+    return 'images/showcase/%s/%s' % (instance.owner.id, filename)
+
+
+class Showcase(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, unique=True)
+    title = models.CharField(max_length=100, help_text='Showcase title')
+    image = models.ImageField(upload_to=upload_showcase_image_to, blank=True)
+    owner = models.ForeignKey(User, help_text='Owner', on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.title
 
 
 class ShowcaseInfo(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, unique=True)
-    title = models.CharField(default='New product', max_length=100, help_text='Product name')
-    description = models.TextField(max_length=4000, blank=True, null=True, help_text='Product description')
+    title = models.CharField(max_length=100, help_text='Info block title')
+    description = models.TextField(max_length=4000, blank=True, null=True, help_text='Info block description')
+    active = models.BooleanField(default=True)
     order = models.PositiveIntegerField(default='0')
-    owner = models.ForeignKey(User, help_text='Product owner', on_delete=models.SET_NULL, null=True)
+    showcase = models.ForeignKey(Showcase, help_text='Showcase', on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.title
@@ -22,12 +37,12 @@ class ShowcaseInfo(models.Model):
 
 class Product(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, unique=True)
-    name = models.CharField(default='Name', max_length=100, help_text='Product name')
+    name = models.CharField(max_length=100, help_text='Product name')
     description = models.TextField(max_length=2000, blank=True, null=True, help_text='Product description')
     price = MoneyField(help_text='Product price', max_digits=14, decimal_places=2, default=0.0, default_currency='RUB')
-    unit_price = models.CharField(default='Unit price', max_length=20, help_text='Product unit price')
+    unit_price = models.CharField(max_length=20, help_text='Product unit price')
     active = models.BooleanField(default=True)
-    image = models.ImageField(upload_to=upload_to, blank=True)
+    image = models.ImageField(upload_to=upload_product_image_to, blank=True)
     #    image = models.ImageField(upload_to='images/products', blank=True)
     owner = models.ForeignKey(User, help_text='Product owner', on_delete=models.SET_NULL, null=True)
 
