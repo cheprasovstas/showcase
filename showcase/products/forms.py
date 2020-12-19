3ad3d3f32401c72b28b76a9f2799b642fcc4bc0a
bro <cheprasov@mail.ru>
2020-12-19 09:47:52 +0300

    save

--- showcase/products/forms.py
+++ showcase/products/forms.py
@@ -23,7 +23,7 @@ class RegistrationFormCustom(RegistrationForm):
 class ProductForm(forms.Form):
 
     name = forms.CharField(label='name',  required=True, max_length=100, widget=forms.TextInput(attrs={'class': "form-control input_form", 'placeholder': "phone"}))
-    description = forms.CharField(label='description', required=False, max_length=2000, widget=forms.Textarea(attrs={'class': "form-control input_form", 'placeholder': "phone"}))
+    description = forms.CharField(label='description', required=False, max_length=2000, widget=forms.Textarea(attrs={'class': "form-control input_form", 'placeholder': ""}))
     price = forms.DecimalField(label='price', required=False, max_digits=5, decimal_places=2, widget=forms.TextInput(attrs={'class': "form-control input_form", 'placeholder': "phone"}))
     unit_price = forms.CharField(label='unit price', required=False, max_length=20, widget=forms.TextInput(attrs={'class': "form-control input_form", 'placeholder': "phone"}))
     active = forms.BooleanField(label='active', initial=True, widget=forms.CheckboxInput())
